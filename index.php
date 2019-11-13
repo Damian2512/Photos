d948c6c90defffff08f643c42ded76f59d19f9bb
Damian <damian@berns.com>
2019-11-13 21:21:33 +0100

    website upgrade

--- index.php
+++ index.php
@@ -1,40 +1,43 @@
-<?php include 'header.php '; ?>
-
 <?php
+// core configuration
+include_once "config/core.php";
 
-function images()
-{
-	$directory = 'images/thumbnails/';
-	if (!is_dir($directory)) {
-		echo 'Directory path is incorrect';
-		return;
-	}
-	$images = glob($directory . "*.{jpg,jpeg,png}", GLOB_BRACE);
+// set page title
+$page_title="Index";
 
-	if (count($images)) {
-		foreach ($images as $image) {
-			echo '<li><img src="' . $image . '" alt=""/></li>' . "\n";
-		}
-	} else {
-		echo '<p>0 images</p>';
-	}
+// include login checker
+$require_login=true;
+include_once "login_checker.php";
+
+// include page header HTML
+include_once 'layout_head.php';
+
+echo "<div class='col-md-12'>";
+
+// to prevent undefined index notice
+$action = isset($_GET['action']) ? $_GET['action'] : "";
+
+// if login was successful
+if($action=='login_success'){
+    echo "<div class='alert alert-info'>";
+    echo "<strong>Hi " . $_SESSION['firstname'] . ", welcome back!</strong>";
+    echo "</div>";
 }
 
+// if user is already logged in, shown when user tries to access the login page
+else if($action=='already_logged_in'){
+    echo "<div class='alert alert-info'>";
+    echo "<strong>You are already logged in.</strong>";
+    echo "</div>";
+}
+
+// content once logged in
+echo "<div class='alert alert-info'>"; ?>
+<?php include 'photo.php'; ?>
+<?php echo "</div>";
+
+echo "</div>";
+
+// footer HTML and JavaScript codes
+include 'layout_foot.php';
 ?>
-
-<div class="main-container main-container_width">
-    <div class="blog-page">
-        <h1 class="page-title">PhotoAlbum</h1>
-        <ul class="gallery-images">
-			<?php
-			images();
-			?>
-        </ul>
-
-        <div class="clear"></div>
-
-    </div> <!-- end blog page -->
-</div>
-<!-- end main container -->
-
-<?php include 'footer.php'; ?>
---
+++ photo.php
@@ -0,0 +1,54 @@
+<?php
+
+function images() {
+
+	$directory = 'images/thumbnails/';
+
+	if(!is_dir($directory)){
+
+		echo 'Directory path is incorrect';
+
+		return;
+
+	}
+
+	$images = glob($directory . "*.{jpg,jpeg,png}", GLOB_BRACE);
+
+
+
+	if (count($images)) {
+
+		foreach ($images as $image) {
+
+			echo '<li><img src="' . $image . '" alt=""/></li>' . "\n";
+
+		}
+
+	}
+
+	else {
+
+		echo '<p>no images available</p>';
+
+	}
+
+}
+
+?>
+
+<section class="container">
+	<div class="row">
+
+		<ul class="gallery-images">
+
+			<?php
+
+			images();
+
+			?>
+
+		</ul>
+
+	</div>
+
+</section>
