1f2ffbec5ce33aa34f607fd50de446c2a98c1ea0
Damian <damian@berns.com>
2019-11-13 21:32:47 +0100

    blank space layout deleted

--- photo.php
+++ photo.php
@@ -3,52 +3,27 @@
 function images() {
 
 	$directory = 'images/thumbnails/';
-
-	if(!is_dir($directory)){
-
+	if(!is_dir($directory)) {
 		echo 'Directory path is incorrect';
-
 		return;
-
 	}
 
 	$images = glob($directory . "*.{jpg,jpeg,png}", GLOB_BRACE);
 
-
-
 	if (count($images)) {
-
 		foreach ($images as $image) {
-
 			echo '<li><img src="' . $image . '" alt=""/></li>' . "\n";
-
 		}
-
-	}
-
-	else {
-
+	} else {
 		echo '<p>no images available</p>';
-
 	}
-
 }
-
 ?>
 
 <section class="container">
-	<div class="row">
-
-		<ul class="gallery-images">
-
-			<?php
-
-			images();
-
-			?>
-
-		</ul>
-
-	</div>
-
+ <div class="row">
+   <ul class="gallery-images">
+			<?php images();	?>
+  </ul>
+ </div>
 </section>
